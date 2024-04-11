1305b5ca2b65f58db4c4791b5f3837ba1c09f32d
Siarhei <mail@mauil.mail>
2024-04-11 19:09:19 +0300

    lesson9 part6

--- src/lesson9/final_key_word/FinalMainClass.java
+++ src/lesson9/final_key_word/FinalMainClass.java
@@ -7,7 +7,6 @@ public class FinalMainClass {
         ParentClass parentClass = new ParentClass();
         parentClass.text = "new text";
         //parentClass.finalText = "new final text";
-
         final String finalText2 = "finalText";
         //finalText2 = finalText2.toLowerCase();
         System.out.println(finalText2);
